53be1b4dd13effdd8d062a1c2a7b854dad66c1ba
sandeepam007 <sandeepam.007@gmail.com>
2022-03-07 15:58:31 +0530

    commiting the new code

--- data_processing.py
+++ data_processing.py
@@ -1,14 +1,12 @@
 
-from re import A
-
-
 print("adding all the code related to data processing")
 
-a= 3
+a = 3
 b = 5
 
 def sum(a,b):
-    c= a + b
+    c = a + b
+
     return a
 
-print("added an incorrect code")
+print("this is an incorrect code")
